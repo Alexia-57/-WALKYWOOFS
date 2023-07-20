3544186a7c7a26ca9df8403f46f707dcdf55ae51
Christopher Ramirez Rodriguez <chrisram415@gmail.com>
2023-07-20 16:03:28 +0900

    added dogwalk, booking, and user validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,32 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :dogwalk
+
+  # Validation for user_id and dogwalk_id, ensuring they are present
+  validates :user_id, :dogwalk_id, presence: true
+
+  # Validation for start_time, ensuring it is present and in the future
+  validates :start_time, presence: true, timeliness: { on_or_after: -> { Time.current } }
+
+  # Validation for total_price, ensuring it is present and a non-negative float value
+  validates :total_price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  # Validation for status, ensuring it is within a specific range of values
+  validates :status, inclusion: { in: 0..2 }
+
+  #  validate for uniqueness in user and dogwalks to prevent registering for the same service twice
+  validates :user_id, uniqueness: {scope: :dogwalk_id, message: "is already in the list"}
 end
+
+
+# #  schema for reference
+# create_table "bookings", force: :cascade do |t|
+#   t.bigint "user_id", null: false
+#   t.bigint "dogwalk_id", null: false
+#   t.datetime "start_time"
+#   t.float "total_price"
+#   t.integer "status", default: 0
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   t.index ["dogwalk_id"], name: "index_bookings_on_dogwalk_id"
+#   t.index ["user_id"], name: "index_bookings_on_user_id"
--- app/models/dogwalk.rb
+++ app/models/dogwalk.rb
@@ -1,4 +1,38 @@
 class Dogwalk < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
+  # has_many :user, through: :bookings, dependent: :destroy
+
+
+  # Validation for service_details, ensuring it is present and not empty
+  validates :service_details, presence: true
+
+  # Validation for dog_capacity, ensuring it is present and a positive integer
+  validates :dog_capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+
+  # Validation for neighborhood, ensuring it is present and not empty
+  validates :neighborhood, presence: true
+
+  # Validation for duration, ensuring it is present and a non-negative float value
+  validates :duration, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  # Validation for hourly_rate, ensuring it is present and a non-negative float value
+  validates :hourly_rate, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  # Validation for user_id, ensuring it is present
+  validates :user_id, presence: true
+
 end
+
+# # schema reference
+# create_table "dogwalks", force: :cascade do |t|
+#   t.text "service_details"
+#   t.integer "dog_capacity"
+#   t.string "neighborhood"
+#   t.float "duration"
+#   t.float "hourly_rate"
+#   t.bigint "user_id", null: false
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+#   t.index ["user_id"], name: "index_dogwalks_on_user_id"
+# end
