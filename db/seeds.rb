9c670f79ebe95020f4edcfc35cddcf9b78f4960b
Christopher Ramirez Rodriguez <chrisram415@gmail.com>
2023-07-29 16:15:08 +0900

    fixed cloudinary

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,35 +44,45 @@ end
 
 puts "Now creating 8 new dogwalk listings..."
 
-options = [0.5, 1, 1.5, 2, 2.5, 3]
+
 
 dogwalks_data = [
-  { user_name: "Zhan", name: "eg1", image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg" },
-  { user_name: "John", name: "eg2", image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg" },
-  { user_name: "Sarah", name: "eg3", image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg" },
-  { user_name: "Yann", name: "eg4", image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg" },
-  { user_name: "Chris", name: "eg5", image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg" },
-  { user_name: "Saki", name: "eg6", image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg" },
-  { user_name: "Alexia", name: "eg7", image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg" },
-  { user_name: "Jane", name: "eg8", image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg" },
+  { user_name: "Zhan", name: "eg1", image_url: "https://cdn.akc.org/content/article-body-image/funny-french_bulldog.jpg" },
+  { user_name: "John", name: "eg2", image_url: "https://e0.pxfuel.com/wallpapers/349/338/desktop-wallpaper-of-dogs-making-funny-faces-cute-funny-dog.jpg" },
+  { user_name: "Sarah", name: "eg3", image_url: "https://e0.pxfuel.com/wallpapers/43/340/desktop-wallpaper-funny-dog-funny-dog-cute-dogs-with-glasses.jpg" },
+  { user_name: "Yann", name: "eg4", image_url: "https://e0.pxfuel.com/wallpapers/151/741/desktop-wallpaper-smiling-dog-dog-smiling-field-green-trees-cute-funny-tongue.jpg" },
+  { user_name: "Chris", name: "eg5", image_url: "https://img.freepik.com/premium-photo/cute-funny-shih-tzu-breed-dog-outdoors-dog-grooming-funny-dog-city_357532-2947.jpg?w=2000" },
+  { user_name: "Saki", name: "eg6", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrqHpbOUZi32jj4q2IifQxDUL-PZ9CQvoeRQ&usqp=CAU" },
+  { user_name: "Alexia", name: "eg7", image_url: "https://i5.walmartimages.com/asr/b1c9258a-09ab-4067-a14f-8778bb192e6d.d879ec718ac734758789398eae06fd61.jpeg" },
+  { user_name: "Jane", name: "eg8", image_url: "https://imgix.ranker.com/user_node_img/3832/76635002/original/menchi-photo-u3?auto=format&q=60&fit=crop&fm=pjpg&dpr=2&w=375" },
 ]
 
 dogwalks_data.each do |dogwalk_info|
   user = User.find_by(nickname: dogwalk_info[:user_name])
 
+  # allow random duration within seeds
+  options = [0.5, 1, 1.5, 2, 2.5, 3]
+
   # Use open-uri to download the image from the provided URL
   image_file = URI.open(dogwalk_info[:image_url])
 
   # Use Cloudinary's uploader to upload the image and get the secure URL
-  cloudinary_response = Cloudinary::Uploader.upload(image_file)
+  # cloudinary_response = Cloudinary::Uploader.upload(image_file)
 
-  Dogwalk.create!(
+  # cloudinary_response = Cloudinary::Uploader.upload(file)
+
+  # debugger
+
+  dogwalk = Dogwalk.new(
     service_details: Faker::Quote.mitch_hedberg,
     dog_capacity: rand(1..5),
     neighborhood: Faker::Address.city,
     duration: options.sample,
     price_per_dog: rand(50..150),
     user: user,
-    photo: cloudinary_response['secure_url']
+    # photo: cloudinary_response['secure_url']
   )
+
+  dogwalk.save!
+  dogwalk.photo.attach(io: image_file, filename: "image.jpg")
 end
