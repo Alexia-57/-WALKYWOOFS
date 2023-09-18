7d171b6ce3d97536494c594e476338623e0a05fe
Christopher Ramirez Rodriguez <chrisram415@gmail.com>
2023-09-18 12:41:13 +0900

    updated seeds to be realistic, changed icon to match jj code, display cloudinary throughout app

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -2,25 +2,39 @@ class BookingsController < ApplicationController
   before_action :set_dogwalk, only: [:create]
 
   def create
-    @booking = Booking.new(booking_params)
-    @booking.user = current_user
-    @booking.dogwalk = @dogwalk
-    # price = @dogwalk.duration.to_i * @dogwalk.hourly_rate.to_i
-    # # adjusted the folowing to account for price_per_dog
-    price = @dogwalk.duration.to_i * @dogwalk.price_per_dog.to_i
-    @booking.total_price = price
-    if @booking.save
-      redirect_to bookings_path
+    if user_signed_in?
+      @booking = Booking.new(booking_params)
+      @booking.user = current_user
+      @booking.dogwalk = @dogwalk
+      # price = @dogwalk.duration.to_i * @dogwalk.hourly_rate.to_i
+      # # adjusted the folowing to account for price_per_dog
+      price = @dogwalk.duration.to_i * @dogwalk.price_per_dog.to_i
+      @booking.total_price = price
+      if @booking.save
+        redirect_to bookings_path
+      else
+        redirect_to dogwalk_path(@dogwalk)
+      end
     else
-      redirect_to dogwalk_path(@dogwalk)
+      # Handle the case when the user is not signed in, sends to sign in
+      redirect_to new_user_session_path, alert: 'Please sign in to make a booking.'
     end
   end
 
   def index
-    @user = current_user
-    mydogwalk = @user.dogwalk
-    @created_dogwalks = @user.bookings
-    @booked_dogwalks = mydogwalk&.bookings
+    # @user = current_user
+    # mydogwalk = @user.dogwalk
+    # @created_dogwalks = @user.bookings
+    # @booked_dogwalks = mydogwalk&.bookings
+    if user_signed_in?
+      @user = current_user
+      mydogwalk = @user.dogwalk
+      @created_dogwalks = @user.bookings
+      @booked_dogwalks = mydogwalk&.bookings
+    else
+      # Handle the case when the user is not signed in, sends to sign in
+      redirect_to new_user_session_path, alert: 'Please sign in to see your bookings.'
+    end
   end
 
   def update
